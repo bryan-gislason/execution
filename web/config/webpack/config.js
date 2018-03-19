7a45e88d3799803a16ec51adaeb0783477bfdac8
Bryan Gislason <bjrgislason@gmail.com>
2018-03-19 10:08:16 -0700

    use ts-loader to biuld

--- web/config/webpack/config.js
+++ web/config/webpack/config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: "inline-source-map",
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
@@ -21,10 +21,9 @@ module.exports = {
 
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
-        loader: "awesome-typescript-loader",
-        options: { config: path.resolve(root, "tsconfig.dist.json") },
+        loader: "ts-loader",
+        options: { configFile: path.resolve(root, "tsconfig.dist.json") },
         test: /\.tsx?$/
       },
 
